9cb6b48bf6b9426f9623a6841a840afb022580e1
Francis Davison <francis.nj.davison@gmail.com>
2021-09-30 22:08:45 +0100

    made progres on Collatz Print code

--- src/Collatz_Print.java
+++ src/Collatz_Print.java
@@ -2,17 +2,36 @@ public class Collatz_Print
 {
 	public static void main(String args[])
 	{
-		int terms = 10;
-		for(int i=0;i<=terms;i++)
+		int terms = 50;
+		System.out.println(terms+": ");
+		for(int i=2;i<=terms;i++)
 		{
+			System.out.println("");
+			System.out.println("seed = "+i+": ");
+			int this_term = i;
+			while(this_term!=1)
+			{
+				System.out.print(this_term+", ");
+				if(this_term%2==0)
+				{
+					this_term=this_term/2;
+				}
+				else
+				{
+					this_term=(3*this_term)+1;
+				}
+				System.out.print("1");
+			}
+			/*
 			if(i%2==0)
 			{
-				System.out.println(i+"even");
+				//System.out.println(i+"even");
 			}
 			else
 			{
-				System.out.println(i+"odd");
+				//System.out.println(i+"odd");
 			}
+			*/
 		}
 	}
 }
