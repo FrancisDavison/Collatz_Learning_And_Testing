882231c69003abea7163f6d62d82bef0500d4f1a
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-15 23:14:19 +0100

    fixed error making it print 1 in the middle of the loop and breaking everything

--- src/Collatz_Print.java
+++ src/Collatz_Print.java
@@ -2,7 +2,7 @@ public class Collatz_Print
 {
 	public static void main(String args[])
 	{
-		int terms = 50;
+		int terms = 100;
 		System.out.println(terms+": ");
 		for(int i=2;i<=terms;i++)
 		{
@@ -11,27 +11,17 @@ public class Collatz_Print
 			int this_term = i;
 			while(this_term!=1)
 			{
-				System.out.print(this_term+", ");
-				if(this_term%2==0)
+				if(this_term%2!=0)
 				{
-					this_term=this_term/2;
+					this_term=(3*this_term)+1;
+					System.out.print(this_term+", ");
 				}
 				else
 				{
-					this_term=(3*this_term)+1;
+					this_term=this_term/2;
+					System.out.print(this_term+", ");
 				}
-				System.out.print("1");
 			}
-			/*
-			if(i%2==0)
-			{
-				//System.out.println(i+"even");
-			}
-			else
-			{
-				//System.out.println(i+"odd");
-			}
-			*/
 		}
 	}
 }
