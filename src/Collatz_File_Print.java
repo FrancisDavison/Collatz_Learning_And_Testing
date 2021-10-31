b32835ed04ab3677f7e04fc599a1a521f74aaa8a
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-31 18:07:04 +0000

    File write working

--- src/Collatz_File_Print.java
+++ src/Collatz_File_Print.java
@@ -1,22 +1,28 @@
+import java.io.IOException;
+
 public class Collatz_File_Print
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
-		int terms=100;
+		int terms=1000;
+		String file_name="C:\\Users\\Janeway\\Dropbox\\Github\\Collatz_Learning_And_Testing\\FileOutputTest\\Collatz_Out.txt";
+		WriteFile data = new WriteFile(file_name, true);
 		for(int i=2;i<=terms;i++)
 		{
+			data.WriteToFile("Seed: "+String.valueOf(i));
 			int this_term=i;
 			while(this_term!=1)
 			{
+				
 				if(this_term%2!=0)
 				{
 					this_term=(3*this_term)+1;
-					//Print term to text file //////////////////////
+					data.WriteToFile(String.valueOf(this_term));
 				}
 				else
 				{
 					this_term=this_term/2;
-					//Print term to text file
+					data.WriteToFile(String.valueOf(this_term));
 				}
 			}
 		}
