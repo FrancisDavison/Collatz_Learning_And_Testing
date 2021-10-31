430a3e7d77cd288edea395989da33b3687e61ebb
FrancisDavison <francis.nj.davison@gmail.com>
2021-10-31 22:39:16 +0000

    set time output to nanotime, ran into issue with last two digits of nanotime outut always 00, fix?

--- src/Collatz_File_Print.java
+++ src/Collatz_File_Print.java
@@ -4,30 +4,30 @@ public class Collatz_File_Print
 {
 	public static void main(String args[]) throws IOException
 	{
-		int terms=10;
+		int terms=10000;
 		String file_name="C:\\Users\\Janeway\\Dropbox\\Github\\Collatz_Learning_And_Testing\\FileOutputTest\\Collatz_Out.txt";
 		WriteFile data = new WriteFile(file_name, true);
-		data.WriteToFile("Program Start: "+String.valueOf(System.currentTimeMillis()));
+		data.WriteToFile("Program Start: "+String.valueOf(System.nanoTime()));
 		for(int i=2;i<=terms;i++)
 		{
 			data.WriteToFile("Seed: "+i);
-			data.WriteToFile("Seed Start: "+String.valueOf(System.currentTimeMillis()));
+			data.WriteToFile("Seed Start: "+String.valueOf(System.nanoTime()));
 			int this_term=i;
 			while(this_term!=1)
 			{
 				if(this_term%2!=0)
 				{
 					this_term=(3*this_term)+1;
-					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.currentTimeMillis()));
+					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.nanoTime()));
 				}
 				else
 				{
 					this_term=this_term/2;
-					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.currentTimeMillis()));
+					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.nanoTime()));
 				}
 			}
-			data.WriteToFile("Seed End: "+String.valueOf(System.currentTimeMillis()));
+			data.WriteToFile("Seed End: "+String.valueOf(System.nanoTime()));
 		}
-		data.WriteToFile("Program End: "+String.valueOf(System.currentTimeMillis()));
+		data.WriteToFile("Program End: "+String.valueOf(System.nanoTime()));
 	}
 }
