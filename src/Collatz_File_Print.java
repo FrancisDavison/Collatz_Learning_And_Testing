31423fc7a9f28c58082129a5e8cba5d8a200fd64
FrancisDavison <francis.nj.davison@gmail.com>
2021-11-01 17:51:16 +0000

    fixed crazy errors with files too big

--- src/Collatz_File_Print.java
+++ src/Collatz_File_Print.java
@@ -7,27 +7,27 @@ public class Collatz_File_Print
 		int terms=10000;
 		String file_name="C:\\Users\\Janeway\\Dropbox\\Github\\Collatz_Learning_And_Testing\\FileOutputTest\\Collatz_Out.txt";
 		WriteFile data = new WriteFile(file_name, true);
-		data.WriteToFile("Program Start: "+String.valueOf(System.nanoTime()));
+		data.WriteToFile("Program Start: "+System.nanoTime());
 		for(int i=2;i<=terms;i++)
 		{
 			data.WriteToFile("Seed: "+i);
-			data.WriteToFile("Seed Start: "+String.valueOf(System.nanoTime()));
+			data.WriteToFile("Seed Start: "+System.nanoTime());
 			int this_term=i;
 			while(this_term!=1)
 			{
 				if(this_term%2!=0)
 				{
 					this_term=(3*this_term)+1;
-					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.nanoTime()));
+					data.WriteToFile(String.valueOf(this_term)+", "+System.nanoTime());
 				}
 				else
 				{
 					this_term=this_term/2;
-					data.WriteToFile(String.valueOf(this_term)+", "+String.valueOf(System.nanoTime()));
+					data.WriteToFile(String.valueOf(this_term)+", "+System.nanoTime());
 				}
 			}
-			data.WriteToFile("Seed End: "+String.valueOf(System.nanoTime()));
+			data.WriteToFile("Seed End: "+System.nanoTime());
 		}
-		data.WriteToFile("Program End: "+String.valueOf(System.nanoTime()));
+		data.WriteToFile("Program End: "+System.nanoTime());
 	}
 }
