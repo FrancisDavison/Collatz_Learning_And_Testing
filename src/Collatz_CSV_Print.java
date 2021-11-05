260da049b0f5be078d9091daf83aa77923c5b2e3
FrancisDavison <francis.nj.davison@gmail.com>
2021-11-05 18:44:33 +0000

    reverted java.IO import on Collatz_File_Print.java, setup code framework for writing to CSV in Collatz_CSV_Print.java

---
+++ src/Collatz_CSV_Print.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+public class Collatz_CSV_Print
+{
+	public static void main(String args[]) throws IOException
+	{
+		int terms=10;
+		System.out.println("Program Start: "+String.valueOf(System.nanoTime()));
+		//Add program start nanoTime to CSV
+		for(int i=2;i<=terms;i++)
+		{
+			//add Seed number to CSV
+			//add Seed start nanoTime to CSV
+			int this_term=i;
+			while(this_term!=1)
+			{
+				if(this_term!=1)
+				{
+					this_term=(3*this_term)+1;
+					//add Intermediary number + nanoTime to CSV
+				}
+				else
+				{
+					this_term=this_term/2;
+					//add Intermediary number + nanoTime to CSV
+				}
+			}
+			//Add seed end nanoTime to CSV
+		}
+		//add Program end nanoTime to CSV
+	}
+	
+}
--- src/Collatz_File_Print.java
+++ src/Collatz_File_Print.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.io.IOException;
 
 public class Collatz_File_Print
 {
