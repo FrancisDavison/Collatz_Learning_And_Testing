47a4a3df856b75525a6648ac32171e782e71d63e
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-20 12:02:30 +0000

    figured out how to modify FilePath on the fly, see File_Path_Modification_Test

--- src/CSV_Test/Collatz_CSV_Print.java
+++ src/CSV_Test/Collatz_CSV_Print.java
@@ -10,8 +10,9 @@ public class Collatz_CSV_Print
 		//Add program start nanoTime to CSV
 		for(int i=2;i<=terms;i++)
 		{
-			//add Seed number to CSV
-			//add Seed start nanoTime to CSV
+			//create CSV with name of current seed number
+			//Write headers to CSV Term, nanoTime
+			//add Seed number and nanoTime to CSV
 			int this_term=i;
 			while(this_term!=1)
 			{
@@ -19,15 +20,22 @@ public class Collatz_CSV_Print
 				{
 					this_term=(3*this_term)+1;
 					//add Intermediary number + nanoTime to CSV
+					continue;
 				}
 				else
 				{
 					this_term=this_term/2;
 					//add Intermediary number + nanoTime to CSV
+					continue;
 				}
 			}
 			//Add seed end nanoTime to CSV
 		}
 		//add Program end nanoTime to CSV
+		//close writer
 	}
 }
+//Name CSV with seed number
+//Calculate this_Term
+//Write this_term and nanoTime to String array
+//Write String array to CSV
---
+++ src/File_Path_Modification_Test.java
@@ -0,0 +1,9 @@
+public class File_Path_Modification_Test
+{
+	public static void main(String args[])
+	{
+		int Seed_Value = 10;
+		String FilePath="C:\\Users\\Janeway\\Dropbox\\Github\\Collatz_Learning_And_Testing\\FileOutputTest\\"+String.valueOf(Seed_Value)+".csv";
+		System.out.println(FilePath);
+	}
+}
