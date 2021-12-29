f91cfe1f3dfd0dae2150d65f9f6971e6e08e3bd2
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-29 14:41:52 +0000

    added Remote_Call package with Remote_Seed_Input and Remote_Seed_Control classes

---
+++ src/Remote_Call/Remote_Seed_Control.java
@@ -0,0 +1,8 @@
+package Remote_Call;
+public class Remote_Seed_Control
+{
+	public static void main(String args[])
+	{
+		
+	}
+}
---
+++ src/Remote_Call/Remote_Seed_Input.java
@@ -0,0 +1,6 @@
+package Remote_Call;
+import java.io.*;
+import com.opencsv.*;
+public class Remote_Seed_Input
+{
+}
