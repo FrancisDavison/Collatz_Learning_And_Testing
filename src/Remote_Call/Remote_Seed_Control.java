2a627dc317c6386564d63dc5c4f107d4cd6112b0
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-29 14:51:35 +0000

    messed with code, more research needed

--- src/Remote_Call/Remote_Seed_Control.java
+++ src/Remote_Call/Remote_Seed_Control.java
@@ -3,6 +3,9 @@ public class Remote_Seed_Control
 {
 	public static void main(String args[])
 	{
-		
+		int Seed=10; //Sets the value of the seed to be computed
+		String FilePath=".\\CSVOutTest\\"+String.valueOf(Seed)+".CSV"; //Sets the file path for the CSV Output
+		//System.out.println(FilePath); //Test Output Line --REMOVE--
+		Remote_Seed_Compute(Seed, FilePath);
 	}
 }
--- src/Remote_Call/Remote_Seed_Input.java
+++ src/Remote_Call/Remote_Seed_Input.java
@@ -3,4 +3,9 @@ import java.io.*;
 import com.opencsv.*;
 public class Remote_Seed_Input
 {
+	public void Remote_Seed_Compute(int Seed, String FilePath)
+	{
+		System.out.println(Seed);
+		System.out.println(FilePath);
+	}
 }
