c5300df6f0efa254d75de095c44c6e2928ff268a
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-31 13:56:47 +0000

    completed Remote_Seed_Compute

--- src/Remote_Call/Remote_Seed_Control.java
+++ src/Remote_Call/Remote_Seed_Control.java
@@ -3,7 +3,7 @@ public class Remote_Seed_Control
 {
 	public static void main(String args[])
 	{
-		int Seed=10; //Sets the value of the seed to be computed by Remote_Seed_Compute
+		int Seed=15; //Sets the value of the seed to be computed by Remote_Seed_Compute
 		if(Remote_Seed_Input.Remote_Seed_Compute(Seed))
 		{
 			System.out.println("Completed Succesfully");
