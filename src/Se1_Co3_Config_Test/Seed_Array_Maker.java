c698ee7394ac314401161e720de9a84662b21f3c
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-01 00:18:44 +0000

    Progress made on Seed_Array_Maker

--- src/Se1_Co3_Config_Test/Seed_Array_Maker.java
+++ src/Se1_Co3_Config_Test/Seed_Array_Maker.java
@@ -1,5 +1,37 @@
 package Se1_Co3_Config_Test;
-
+import java.util.*;
 public class Seed_Array_Maker
 {
+	public static void main(String args[])
+	{
+		Scanner Input_Nodes = new Scanner(System.in);
+		Scanner Input_Seeds = new Scanner(System.in);
+		System.out.print("Input number of compute nodes: ");
+		int Compute_Nodes = Input_Nodes.nextInt();
+		System.out.println("");
+		System.out.print("Input number of seeds: ");
+		int Seeds = Input_Seeds.nextInt();
+		
+		String[] Node_Names = new String[Compute_Nodes];
+		Node_Names[0]="Node1";
+		System.out.println(Node_Names[0]);
+		
+		
+		
+		
+		
+		
+		
+		HashMap<String,int[]> arraynames = new HashMap<String,int[]>();
+		arraynames.put("fruits", new int[10]);
+
+		// then simply access it with
+		arraynames.get("fruits")[0] = 100;
+		
+		System.out.println(Arrays.toString(arraynames.get("fruits")));
+		
+		Input_Nodes.close();
+		Input_Seeds.close();
+	}
+	
 }
