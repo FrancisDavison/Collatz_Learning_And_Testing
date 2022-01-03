1e318390ba184c1a0fa7195e62aef4d7dd14b9bd
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-03 12:59:42 +0000

    progress made on Seed_Array_Maker

--- src/Se1_Co3_Config_Test/Seed_Array_Maker.java
+++ src/Se1_Co3_Config_Test/Seed_Array_Maker.java
@@ -12,37 +12,45 @@ public class Seed_Array_Maker
 		System.out.print("Input number of seeds: "); //Asking user
 		int Seeds = Input_Seeds.nextInt(); //Saving user input for number of seeds as Seeds
 		
-		String[] Node_Names = new String[Compute_Nodes]; //Setting up String array to store Compute Node hostnames
-		Node_Names[0]="Node1"; //Adding test hostname to array
-		System.out.println(Node_Names[0]);//Print array, just for testing
+		//String[] Node_Names = new String[Compute_Nodes]; //Setting up String array to store Compute Node hostnames
+		//Node_Names[0]="Node1"; //Adding test hostname to array
+		//System.out.println(Node_Names[0]);//Print array, just for testing
 		
 		int Rows = Compute_Nodes; //Sets the number of rows in Seed_Table array to number of compute nodes, so each node has one row and all seeds for that node are stored only in that row
 		int Seeds_Remainder = Seeds%Compute_Nodes; //Calculating Seeds_Remainder
-		int Columns = ((Seeds+Seeds_Remainder)/(Compute_Nodes)); //Calculating number of columns using seeds+seeds remainder, this allows there to always be an integer number of columns and prevents the code from exploding
-		System.out.println(Columns); //Printing number of columns, just for testing
+		int Columns = ((Seeds+Seeds_Remainder)/(Compute_Nodes))+1; //Calculating number of columns using seeds+seeds remainder, this allows there to always be an integer number of columns and prevents the code from exploding
 		int[][] Seed_Table = new int[Rows][Columns]; //Create Seed_Table array of integers, this will store all seeds needed in the computation, will need to test maximum size
-		System.out.println(Arrays.deepToString(Seed_Table)); //Prints Seed_Table, just for testing
+		
+		//Backfill final row with -1
+		for(int i=0;i<Rows;i++)
+		{
+			Seed_Table[i][Columns-1]=-1;
+		}
+		
+		int Current_Seed=2;
+		for(int w=0;w<(Columns-1);w++)
+		{
+			for(int x=0;x<Rows;x++)
+			{
+				Seed_Table[x][w]=Current_Seed;
+				Current_Seed+=1;
+				if(Current_Seed==Seeds)
+				{
+					break;
+				}
+			}
+		}
 		
 		
 		
 		
 		
 		
-	
 		
-		
-		
-		
-		
-		/*
-		HashMap<String,int[]> arraynames = new HashMap<String,int[]>();
-		arraynames.put("fruits", new int[10]);
-
-		// then simply access it with
-		arraynames.get("fruits")[0] = 100;
-		
-		System.out.println(Arrays.toString(arraynames.get("fruits")));
-		*/
+		for(int a=0;a<Rows;a++) //This snippet will just print the Seed_Table out in a more understandable format, only for testing
+		{
+			System.out.println(Arrays.toString(Seed_Table[a]));
+		}
 		
 		Input_Nodes.close();
 		Input_Seeds.close();
