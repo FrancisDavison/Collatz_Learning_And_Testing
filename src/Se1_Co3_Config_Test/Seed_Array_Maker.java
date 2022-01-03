74a124efcb14b227361cc9e83cd4ef0cbee26e10
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-03 12:05:20 +0000

    Code to create Seed_Table complete

--- src/Se1_Co3_Config_Test/Seed_Array_Maker.java
+++ src/Se1_Co3_Config_Test/Seed_Array_Maker.java
@@ -12,17 +12,16 @@ public class Seed_Array_Maker
 		System.out.print("Input number of seeds: "); //Asking user
 		int Seeds = Input_Seeds.nextInt(); //Saving user input for number of seeds as Seeds
 		
-		String[] Node_Names = new String[Compute_Nodes];
-		Node_Names[0]="Node1";
-		System.out.println(Node_Names[0]);
-		int Rows = Compute_Nodes;
-		int Seeds_Remainder = Seeds%Compute_Nodes;
-		int Columns = ((Seeds+Seeds_Remainder)/(Compute_Nodes));
-		System.out.println(Columns);
+		String[] Node_Names = new String[Compute_Nodes]; //Setting up String array to store Compute Node hostnames
+		Node_Names[0]="Node1"; //Adding test hostname to array
+		System.out.println(Node_Names[0]);//Print array, just for testing
 		
-		int[][] Seed_Table = new int[Rows][Columns];
-		
-		System.out.println(Arrays.deepToString(Seed_Table));
+		int Rows = Compute_Nodes; //Sets the number of rows in Seed_Table array to number of compute nodes, so each node has one row and all seeds for that node are stored only in that row
+		int Seeds_Remainder = Seeds%Compute_Nodes; //Calculating Seeds_Remainder
+		int Columns = ((Seeds+Seeds_Remainder)/(Compute_Nodes)); //Calculating number of columns using seeds+seeds remainder, this allows there to always be an integer number of columns and prevents the code from exploding
+		System.out.println(Columns); //Printing number of columns, just for testing
+		int[][] Seed_Table = new int[Rows][Columns]; //Create Seed_Table array of integers, this will store all seeds needed in the computation, will need to test maximum size
+		System.out.println(Arrays.deepToString(Seed_Table)); //Prints Seed_Table, just for testing
 		
 		
 		
