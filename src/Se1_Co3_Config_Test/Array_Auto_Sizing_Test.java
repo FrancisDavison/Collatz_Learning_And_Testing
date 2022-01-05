3ef4be971bd06c745f283b63673f1651718c8ff7
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-05 22:32:36 +0000

    end of day commit

--- src/Se1_Co3_Config_Test/Array_Auto_Sizing_Test.java
+++ src/Se1_Co3_Config_Test/Array_Auto_Sizing_Test.java
@@ -1,10 +1,14 @@
 package Se1_Co3_Config_Test;
-import java.io.*;
+import java.util.*;
 public class Array_Auto_Sizing_Test
 {
 	public static void main(String args)
 	{
 		Scanner Input_Nodes = new Scanner(System.in);
 		Scanner Input_Seeds = new Scanner(System.in);
+		
+		
+		Input_Nodes.close();
+		Input_Seeds.close();
 	}
 }
