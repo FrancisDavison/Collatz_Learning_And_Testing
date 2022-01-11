7cb89921d9a6ea8c6c00a8c2ab392ccaf5da736a
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-11 15:33:12 +0000

    added Seed_Array_Alternate to test alternate algorithm for generating seed array

---
+++ src/Seed_Array_Testing/Seed_Array_Alternate.java
@@ -0,0 +1,19 @@
+package Seed_Array_Testing;
+import java.util.*;
+import java.util.Scanner;
+
+public class Seed_Array_Alternate
+{
+	public static void main(String args[])
+	{
+		Scanner Input_Nodes = new Scanner(System.in); //Starting input scanner to get number of Input Nodes from user
+		Scanner Input_Seeds = new Scanner(System.in); //Starting input scanner to get number of Input Seeds from user
+		System.out.print("Input number of compute nodes: "); //Asking user
+		int Compute_Nodes = Input_Nodes.nextInt(); //Saving user input for number of compute nodes as Compute_Nodes
+		System.out.println(""); //Adding a line, just makes it look a little nicer
+		System.out.print("Input number of seeds: "); //Asking user
+		int Seeds = Input_Seeds.nextInt(); //Saving user input for number of seeds as Seeds
+		
+		
+	}
+}
