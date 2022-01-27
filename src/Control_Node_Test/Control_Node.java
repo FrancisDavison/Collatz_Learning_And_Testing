8ea48774953435560217dfba9455434d9470d718
FrancisDavison <francis.nj.davison@gmail.com>
2022-01-27 23:18:28 +0000

    end of day commit

--- src/Control_Node_Test/Control_Node.java
+++ src/Control_Node_Test/Control_Node.java
@@ -1,5 +1,14 @@
 package Control_Node_Test;
-
+import java.io.*;
+import java.net.*;
 public class Control_Node
 {
+	public static void main(String args[])
+	{
+		ServerSocket ControlSocket=null; //Declare ControlSocket and set to null, allows the control node to
+		boolean listening=true; //Declares boolean listening as true, sets Server to default always listen on ports
+		String ControlName="Control"; //Declares ControlName variable and sets equal to Control, this names the server
+		int ControlNum=4545; //Declares ControlNum variable, this sets the socket number that the server will listen on
+		
+	}
 }
