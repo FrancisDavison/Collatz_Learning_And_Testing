079f308077cae4772c7b8ec9c801d9d4038068cf
FrancisDavison <francis.nj.davison@gmail.com>
2022-02-03 15:44:24 +0000

    made progress on Control_State class

--- src/Control_Node_Test/Control_State.java
+++ src/Control_Node_Test/Control_State.java
@@ -2,5 +2,30 @@ package Control_Node_Test;
 
 public class Control_State
 {
-	
+	private Control_State Shared_Objecct;
+	private String Thread_Name;
+	private boolean Current_Access=false;
+	private int Waiting=0;
+	private int[][] Seed_Table;
+	//Construcor
+	Control_State(int[][] Seed_Table_In)
+	{
+		Seed_Table=Seed_Table_In;
+	}
+	//Attempt to acquire lock
+	public synchronized void AcquireLock() throws InterruptedException
+	{
+		Thread  me=Thread.currentThread(); //====Need a better name than me====
+		System.out.println(me.getName()+" is attempting to acquire a lock");
+		++Waiting;
+		while(Current_Access)
+		{
+			System.out.println(me.getName()+" waiting to get a lock, someone else is accessing...");
+			wait();
+		}
+		//nobody currently has a lock, so assign lock to current thread
+		--Waiting;
+		Current_Access=true;
+		System.out.println(me.getName()+" got a lock!");
+	}
 }
