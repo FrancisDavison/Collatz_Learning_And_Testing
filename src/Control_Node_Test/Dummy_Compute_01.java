0128766fa8db2c99ecf936247435b89292332a00
FrancisDavison <francis.nj.davison@gmail.com>
2022-02-13 15:07:55 +0000

    progress made on Dummy_Compute_01

--- src/Control_Node_Test/Dummy_Compute_01.java
+++ src/Control_Node_Test/Dummy_Compute_01.java
@@ -3,4 +3,22 @@ import java.io.*;
 import java.net.*;
 public class Dummy_Compute_01
 {
+	public static void main(String args[]) throws IOException
+	{
+		Socket Compute_Socket=null;
+		PrintWriter out=null;
+		BufferedReader in=null;
+		int Control_Socket_Num=4545;
+		String Control_Name="localhost";
+		String Compute_Node_Id="DummyCompute1";
+		try
+		{
+		}
+		catch(UnknownHostException e)
+		{
+		}
+		catch(IOException e)
+		{
+		}
+	}
 }
