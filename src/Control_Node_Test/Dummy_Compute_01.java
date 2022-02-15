ce0ced72c5eff47fceb86c1efb3aded888b07884
FrancisDavison <francis.nj.davison@gmail.com>
2022-02-15 17:50:07 +0000

    Completed framework of Dummy_Compute_01

--- src/Control_Node_Test/Dummy_Compute_01.java
+++ src/Control_Node_Test/Dummy_Compute_01.java
@@ -13,12 +13,39 @@ public class Dummy_Compute_01
 		String Compute_Node_Id="DummyCompute1";
 		try
 		{
+			Compute_Socket=new Socket(Control_Name,Control_Socket_Num);
+			out=new PrintWriter(Compute_Socket.getOutputStream(), true);
+			in=new BufferedReader(new InputStreamReader(Compute_Socket.getInputStream()));
 		}
+		
 		catch(UnknownHostException e)
 		{
+			System.err.println("Dont know about host: localhost");
+			System.exit(1);
 		}
+		
 		catch(IOException e)
 		{
+			System.err.println("Couldn't get the I/O connection to: "+Control_Socket_Num);
+			System.exit(1);
+		}
+		
+		BufferedReader stdIn=new BufferedReader(new InputStreamReader(System.in));
+		String From_Server;
+		String From_User;
+		
+		System.out.println("Initialised "+Compute_Node_Id+" client and I/O connections");
+		
+		while(true)
+		{
+			From_User=stdIn.readLine();
+			if(From_User!=null)
+			{
+				System.out.println(Compute_Node_Id+" sending "+From_User+" to Control Node");
+				out.println(From_User);
+			}
+			From_Server=in.readLine();
+			System.out.println(Compute_Node_Id+" recieved "+From_Server+" from Control Node");
 		}
 	}
 }
